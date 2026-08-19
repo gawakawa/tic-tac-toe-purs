8c6bb594079fae8a291cf1bacf16d2aeb933f867
gawakawa <mashurakiryu@icloud.com>
2026-08-20 01:04:06 +0900

    :sparkles: Render 3x3 tic-tac-toe board

--- src/Main.purs
+++ src/Main.purs
@@ -5,7 +5,7 @@ import Prelude
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Exception (throw)
-import React.Basic (JSX)
+import React.Basic (JSX, fragment)
 import React.Basic.DOM as R
 import React.Basic.DOM.Client (createRoot, renderRoot)
 import Web.DOM.NonElementParentNode (getElementById)
@@ -13,8 +13,33 @@ import Web.HTML (window)
 import Web.HTML.HTMLDocument (toNonElementParentNode)
 import Web.HTML.Window (document)
 
-square :: JSX
-square = R.button { className: "square", children: [ R.text "X" ] }
+board :: JSX
+board = fragment
+  [ R.div
+      { className: "board-row"
+      , children:
+          [ R.button { className: "square", children: [ R.text "1" ] }
+          , R.button { className: "square", children: [ R.text "2" ] }
+          , R.button { className: "square", children: [ R.text "3" ] }
+          ]
+      }
+  , R.div
+      { className: "board-row"
+      , children:
+          [ R.button { className: "square", children: [ R.text "4" ] }
+          , R.button { className: "square", children: [ R.text "5" ] }
+          , R.button { className: "square", children: [ R.text "6" ] }
+          ]
+      }
+  , R.div
+      { className: "board-row"
+      , children:
+          [ R.button { className: "square", children: [ R.text "7" ] }
+          , R.button { className: "square", children: [ R.text "8" ] }
+          , R.button { className: "square", children: [ R.text "9" ] }
+          ]
+      }
+  ]
 
 main :: Effect Unit
 main = do
@@ -24,4 +49,4 @@ main = do
     Nothing -> throw "Could not find container element"
     Just container -> do
       reactRoot <- createRoot container
-      renderRoot reactRoot square
+      renderRoot reactRoot board
