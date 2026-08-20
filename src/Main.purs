e0bb9be1af8538c19b810c97812cbc6a406b4d4a
gawakawa <mashurakiryu@icloud.com>
2026-08-20 22:41:11 +0900

    :sparkles: Lift squares state up to the board

--- src/Main.purs
+++ src/Main.purs
@@ -2,6 +2,7 @@ module Main where
 
 import Prelude
 
+import Data.Array (replicate, updateAt, (!!))
 import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Tuple.Nested ((/\))
 import Effect (Effect)
@@ -17,40 +18,51 @@ import Web.HTML (window)
 import Web.HTML.HTMLDocument (toNonElementParentNode)
 import Web.HTML.Window (document)
 
-mkSquare :: Component Unit
-mkSquare = component "Square" \_ ->
-  React.do
-    value /\ setValue <- useState' Nothing
-    let
-      handleClick :: Effect Unit
-      handleClick = setValue $ Just "X"
-    pure $ R.button
-      { className: "square"
-      , onClick: handler_ handleClick
-      , children: [ R.text $ fromMaybe "" value ]
-      }
+square :: Maybe String -> Effect Unit -> JSX
+square value onSquareClick = R.button
+  { className: "square"
+  , onClick: handler_ onSquareClick
+  , children: [ R.text $ fromMaybe "" value ]
+  }
 
-mkBoard :: (Unit -> JSX) -> Component Unit
-mkBoard square = component "Board" \_ ->
-  pure $ fragment
-    [ R.div
-        { className: "board-row"
-        , children: [ square unit, square unit, square unit ]
-        }
-    , R.div
-        { className: "board-row"
-        , children: [ square unit, square unit, square unit ]
-        }
-    , R.div
-        { className: "board-row"
-        , children: [ square unit, square unit, square unit ]
-        }
-    ]
+mkBoard :: Component Unit
+mkBoard = component "Board" \_ ->
+  React.do
+    squares /\ setSquares <- useState' $ replicate 9 Nothing
+    let
+      handleClick :: Int -> Effect Unit
+      handleClick i = setSquares $ fromMaybe squares $ updateAt i (Just "X")
+        squares
+    pure $ fragment
+      [ R.div
+          { className: "board-row"
+          , children:
+              [ square (join $ squares !! 0) (handleClick 0)
+              , square (join $ squares !! 1) (handleClick 1)
+              , square (join $ squares !! 2) (handleClick 2)
+              ]
+          }
+      , R.div
+          { className: "board-row"
+          , children:
+              [ square (join $ squares !! 3) (handleClick 3)
+              , square (join $ squares !! 4) (handleClick 4)
+              , square (join $ squares !! 5) (handleClick 5)
+              ]
+          }
+      , R.div
+          { className: "board-row"
+          , children:
+              [ square (join $ squares !! 6) (handleClick 6)
+              , square (join $ squares !! 7) (handleClick 7)
+              , square (join $ squares !! 8) (handleClick 8)
+              ]
+          }
+      ]
 
 main :: Effect Unit
 main = do
-  square <- mkSquare
-  board <- mkBoard square
+  board <- mkBoard
   doc <- document =<< window
   root <- getElementById "root" $ toNonElementParentNode doc
   case root of
